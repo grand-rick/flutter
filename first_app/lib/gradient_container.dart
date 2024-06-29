d12f133affb2abd06c62da8d90d0db2eb990554a
Patrick Murimi <kabugap02@gmail.com>
2024-06-29 03:51:11 +0300

    feat: Add an image; learn about multi constructor functions

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -1,29 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:first_app/styled_text.dart';
+// import 'package:first_app/styled_text.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer(this.color1, this.color2,
-      {super.key, required this.color3});
-
   final Color color1;
   final Color color2;
-  final MaterialColor color3;
+
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  // const GradientContainer.purple({super.key})
+  //     : color1 = Colors.deepPurple,
+  //       color2 = Colors.deepPurpleAccent;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [color1, color3, color2],
+          colors: [color1, color2],
           begin: startAlignment,
           end: endAlignment,
         ),
       ),
-      child: const Center(
-        child: StyledText('Thanks O\'Reilly!'),
+      child: Center(
+        child: Image.asset(
+          './assets/images/dice-5.png',
+          width: 200,
+          height: 200,
+        ),
       ),
     );
   }
--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -4,15 +4,11 @@ import 'package:first_app/gradient_container.dart';
 void main() {
   const Color color1 = Color.fromARGB(255, 50, 6, 126);
   const Color color2 = Color.fromARGB(255, 162, 18, 184);
-  const MaterialColor color3 = Colors.deepPurple;
   runApp(
     const MaterialApp(
       home: Scaffold(
-        body: GradientContainer(
-          color1,
-          color2,
-          color3: color3,
-        ),
+        // body: GradientContainer.purple(),
+        body: GradientContainer(color1, color2),
       ),
     ),
   );
