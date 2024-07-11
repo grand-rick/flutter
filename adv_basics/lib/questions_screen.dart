7fba0c5fb5e92d73d241173a2b069c2a9a9d8d2c
Patrick Murimi <kabugap02@gmail.com>
2024-07-11 22:58:06 +0300

    chore: accessed questions manually

--- adv_basics/lib/questions_screen.dart
+++ adv_basics/lib/questions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:adv_basics/answer_button.dart';
 import 'package:flutter/material.dart';
+import 'package:adv_basics/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -13,23 +14,23 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
+    final currentQuestion = questions[0];
     return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            'The Question...',
-            style: TextStyle(
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(
               color: Colors.white,
-              fontSize: 24,
             ),
           ),
           const SizedBox(height: 30),
-          AnswerButton('Answer 1', () {}),
-          AnswerButton('Answer 2', () {}),
-          AnswerButton('Answer 3', () {}),
-          AnswerButton('Answer 4', () {}),
+          AnswerButton(currentQuestion.answers[0], () {}),
+          AnswerButton(currentQuestion.answers[1], () {}),
+          AnswerButton(currentQuestion.answers[2], () {}),
+          AnswerButton(currentQuestion.answers[3], () {}),
         ],
       ),
     );
